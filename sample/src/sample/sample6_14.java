87f20a3afa1f8e07a3cbb8ba8ed869e64b16031c
schwarzwald0906@gmail.com <schwarzwald0906@gmail.com>
2022-04-21 22:49:36 +0900

    ４月第３週

--- sample/src/sample/sample6_14.java
+++ sample/src/sample/sample6_14.java
@@ -2,4 +2,11 @@ package sample;
 
 public class sample6_14 {
 
+	void method(void) {}
+	void method(int ...values)
+	void method(int... values,String name) {}
+	void method(int a,int...b) {}
+	
+	
+	
 }
